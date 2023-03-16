0f87b6918d3d75e9a0a5558d975bf8cea7636b86
LiquidPlummer <kyle.plummer@revature.com>
2023-03-16 14:26:27 -0400

    test cases added to main

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -6,9 +6,36 @@ public class TestCases {
 
     @Test
     public void testCase1() {
-        int[] test = new int[]{-1,0,3,5,9,12};
-        int target = 9;
+        int[] test = new int[]{-4,-3,-2,-1,0,1,2,3,4};
+        int target = 0;
         int expected = 4;
+
+        Assertions.assertEquals(expected, solution.search(test, target));
+    }
+
+    @Test
+    public void testCase2() {
+        int[] test = new int[]{};
+        int target = 2;
+        int expected = -1;
+
+        Assertions.assertEquals(expected, solution.search(test, target));
+    }
+
+    @Test
+    public void testCase3() {
+        int[] test = new int[]{-3,-2,-1};
+        int target = 2;
+        int expected = -1;
+
+        Assertions.assertEquals(expected, solution.search(test, target));
+    }
+
+    @Test
+    public void testCase4() {
+        int[] test = new int[]{-3,-2,-1};
+        int target = 2;
+        int expected = -1;
 
         Assertions.assertEquals(expected, solution.search(test, target));
     }
