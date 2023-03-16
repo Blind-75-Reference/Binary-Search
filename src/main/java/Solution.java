99789d189513700ba5400c87f6c3cc5846d8711e
LiquidPlummer <kyle.plummer@revature.com>
2023-03-16 14:25:51 -0400

    solution 1

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
+        int leftBoundary = 0;
+        int rightBoundary = nums.length-1;
 
+        while(leftBoundary <= rightBoundary) {
+            int mid = (rightBoundary - leftBoundary) / 2 + leftBoundary;
+            if(nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                //target is left of mid
+                rightBoundary = mid-1;
+            } else {
+                //target is right of mid
+                leftBoundary = mid+1;
+            }
+        }
         return -1;
     }
 }
